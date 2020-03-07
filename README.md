f2e2d599ecbce2725b356e71c74c5f1e7ea111eb
christinecoding1 <60949965+christinecoding1@users.noreply.github.com>
2020-03-06 22:43:37 -0500

    final commit, addEventListener fixed

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 
-Create a new repo for this project on GitHub.
-Create a README.md file for your repo. Add details about your project to the README, for example:
-A description of the project in your own words
-The skills, techniques and process used to complete the project
-If you’ve attempted to earn an “Exceeds Expectations” grade, list the “Extra Credit” features you added.
-If you need a reminder on how to use GitHub and GitHub desktop, please refer back to the workshop, Share Your Projects with GitHub Desktop.
+Project 2: List Pagination and Filtering 
+
+This project uses the "pagination" technique in JavaScript to enhance the usablility of the webpage. The HTML document contains a list of 54 student names, photos and email addresses. The JavaScript creates a series of links at the bottom of the web page that lets users click through the list of students, 10 entries at a time. The JavaScript code is designed to work with a list of any number of students, not just the 54 in this project.
--- js/script.js
+++ js/script.js
@@ -1,53 +1,30 @@
 /******************************************
-Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project 
-//- https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const studentList = document.getElementsByClassName('student-item'); //variable that stores the list of students located in the HTML file 
+const itemsPerPage = 10; //variable that stores the number of items that will be shown on each page 
 
-
-const studentList = document.getElementsByTagName('li');
-const itemsPerPage = 10;
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-***/
-
-const showPage = (list, page) => {
+//function that hides all of the items in the student list except for the ten that want to show
+const showPage = (list, page) => { //the function has a list of items ('list') and the number of the page that should be shown ('page') as parameters
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = page * itemsPerPage;
 
-   for (let i=0; i<list.length; i++){
-      if (i>=startIndex && i<endIndex){
+   for (let i=0; i<list.length; i++){ //a for loop that loops over all the items in the 'list' parameter to make the browser only display the 10 student items of the 'page' parameter 
+      if (i>=startIndex && i<endIndex){ 
          list[i].style.display = '';
       } else {
          list[i].style.display = 'none';
-       }
+      }
    }
 };
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
+//function that generates, appends, and adds functionality to the pagination buttons
 const appendPageLinks = (list) => {
-   const numPages = Math.ceil(studentList.length/itemsPerPage);
+   //function's parameter represents the list of students 
+   const numPages = Math.ceil(studentList.length/itemsPerPage); //variable that determines how many link pages are needed for the given list 
+   //creation of <a>, <li>, <ul>, and <div> elements and appending them to the appropriate <div> element already in the HTML
    const paginationDiv = document.createElement('div');
    paginationDiv.className = 'pagination';
    const div = document.querySelector('.page');
@@ -56,30 +33,32 @@ const appendPageLinks = (list) => {
    paginationDiv.appendChild(paginationUl);
  
    for (let i=1; i<=numPages; i++){
+      //a for loop that adds <li> and <a> tags with a text content of i to​ every page
       const paginationLi= document.createElement('li');
       const paginationLink= document.createElement('a');
       paginationUl.appendChild(paginationLi);
       paginationLi.appendChild(paginationLink);
       paginationLink.textContent = i;
       
+      //conditional statement so that initially the first pagination link's class name is 'active' (to activate certain CSS style properties for the link)
       if (i===1){
          paginationLink.className='active';
          } else {
-            paginationLink.className='';
+         paginationLink.className='';
          }
-
-         paginationLink.addEventListener("click", (event) => {
-            if (event.target.tagName === 'a'){ 
-               paginationLink[i].className = ' ';
-               event.target.className = "active";
-               showPage(list, paginationLink.textContent);
-               console.log(event.target.tagName);
-            }
-         });
-      }
+         /*creation of an event listener for each <a>​ ​tag. When an <a> element is clicked the showPage function is called so 
+         that only the student names of the clicked link are displayed */
+         paginationLink.addEventListener('click', (event) => {
+         const lastActive = document.getElementsByClassName('active'); /* variable to select all the elements that have a class name of 'active' 
+         (Only one element has a class of 'active' at any given time) */
+         lastActive[0].className = lastActive[0].className.replace('active', ''); //for the element with a class of 'active', make it's class name ' '
+         event.target.className = 'active'; //changing the class name of the element clicked to 'active'
+         showPage(list, paginationLink.textContent);
+      });
+   }
 };
 
+//calling both functions so that when the web page is loaded the links are appended to the page and the first 10 student items are shown
 showPage(studentList, 1);
 appendPageLinks(studentList);
-// Remember to delete the comments that came with this file, 
-// and replace them with your own code comments
+
