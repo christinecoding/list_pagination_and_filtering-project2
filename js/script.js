63615261c7fbc0267688e6ad1715a2e6db621280
christinecoding1 <60949965+christinecoding1@users.noreply.github.com>
2020-03-07 19:22:00 -0500

    update to code comments

--- js/script.js
+++ js/script.js
@@ -7,11 +7,13 @@ const studentList = document.getElementsByClassName('student-item'); //variable 
 const itemsPerPage = 10; //variable that stores the number of items that will be shown on each page 
 
 //function that hides all of the items in the student list except for the ten that want to show
-const showPage = (list, page) => { //the function has a list of items ('list') and the number of the page that should be shown ('page') as parameters
+//the function has a list of items ('list') and the number of the page that should be shown ('page') as parameters
+const showPage = (list, page) => { 
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = page * itemsPerPage;
 
-   for (let i=0; i<list.length; i++){ //a for loop that loops over all the items in the 'list' parameter to make the browser only display the 10 student items of the 'page' parameter 
+   //a for loop that loops over all the items in the 'list' parameter to make the browser only display the 10 student items of the 'page' parameter
+   for (let i=0; i<list.length; i++){  
       if (i>=startIndex && i<endIndex){ 
          list[i].style.display = '';
       } else {
@@ -21,8 +23,8 @@ const showPage = (list, page) => { //the function has a list of items ('list') a
 };
 
 //function that generates, appends, and adds functionality to the pagination buttons
+//function's parameter represents the list of students 
 const appendPageLinks = (list) => {
-   //function's parameter represents the list of students 
    const numPages = Math.ceil(studentList.length/itemsPerPage); //variable that determines how many link pages are needed for the given list 
    //creation of <a>, <li>, <ul>, and <div> elements and appending them to the appropriate <div> element already in the HTML
    const paginationDiv = document.createElement('div');
@@ -32,8 +34,8 @@ const appendPageLinks = (list) => {
    const paginationUl = document.createElement('ul');
    paginationDiv.appendChild(paginationUl);
  
+   //a for loop that adds <li> and <a> tags with a text content of i to​ every page
    for (let i=1; i<=numPages; i++){
-      //a for loop that adds <li> and <a> tags with a text content of i to​ every page
       const paginationLi= document.createElement('li');
       const paginationLink= document.createElement('a');
       paginationUl.appendChild(paginationLi);
@@ -46,14 +48,14 @@ const appendPageLinks = (list) => {
          } else {
          paginationLink.className='';
          }
-         /*creation of an event listener for each <a>​ ​tag. When an <a> element is clicked the showPage function is called so 
-         that only the student names of the clicked link are displayed */
-         paginationLink.addEventListener('click', (event) => {
-         const lastActive = document.getElementsByClassName('active'); /* variable to select all the elements that have a class name of 'active' 
-         (Only one element has a class of 'active' at any given time) */
-         lastActive[0].className = lastActive[0].className.replace('active', ''); //for the element with a class of 'active', make it's class name ' '
-         event.target.className = 'active'; //changing the class name of the element clicked to 'active'
-         showPage(list, paginationLink.textContent);
+      /*creation of an event listener for each <a>​ ​tag. When an <a> element is clicked the showPage function is called so 
+      that only the student names of the clicked link are displayed */
+      paginationLink.addEventListener('click', (event) => {
+      //variable to select all the elements that have a class name of 'active' (Only one element has a class of 'active' at any given time)//
+      const lastActive = document.getElementsByClassName('active'); 
+      lastActive[0].className = lastActive[0].className.replace('active', ''); //for the element with a class of 'active', make it's class name ''
+      event.target.className = 'active'; //changing the class name of the element clicked to 'active'
+      showPage(list, paginationLink.textContent);
       });
    }
 };
